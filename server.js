9f164dfff3610ed712d4ab4fbc749e74cb265a7e
John <jlev2282@gmail.com>
2017-08-11 00:36:43 -0400

    trying to change the env argument in server.js

--- server.js
+++ server.js
@@ -31,15 +31,16 @@ app.use(express.static("public"));
 
 // Database configuration with mongoose
 var databaseUri = 'mongodb://localhost/ajcscraper';
-
-mongoose.connect("mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93");
+var herokuconnect = "mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93";
+// mongoose.connect("mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93");
 
 if (process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI);
 } else {
     mongoose.connect(databaseUri);
-var db = mongoose.connection;
 }
+
+var db = mongoose.connection;
 
 // Show any mongoose errors
 db.on("error", function(error) {
