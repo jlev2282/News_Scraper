20cf1a04e40fbe2410ecea6c9b14e2899182eb03
John <jlev2282@gmail.com>
2017-08-11 00:57:52 -0400

    reversion

--- server.js
+++ server.js
@@ -32,13 +32,13 @@ app.use(express.static("public"));
 // Database configuration with mongoose
 var databaseUri = 'mongodb://localhost/ajcscraper';
 var herokuconnect = "mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93";
-// mongoose.connect("mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93");
+mongoose.connect("mongodb://heroku_5r5qqp93:g7klub7e9tr3265h7htkusscgg@ds153392.mlab.com:53392/heroku_5r5qqp93");
 
-if (herokuconnect) {
-    mongoose.connect(herokuconnect);
-} else {
-    mongoose.connect(databaseUri);
-}
+// if (herokuconnect) {
+//     mongoose.connect(herokuconnect);
+// } else {
+//     mongoose.connect(databaseUri);
+// }
 
 var db = mongoose.connection;
 
